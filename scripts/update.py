9d2a95e43ba3966594287beccc91b07e4e2cf7ad
Hubert Wajda <hubert.wajda03@gmail.com>
2024-11-22 21:12:14 +0100

    creating update.py

--- scripts/update.py
+++ scripts/update.py
@@ -1,11 +1,9 @@
 import os
 import requests
 import requests.auth
-import warnings
 import xml.etree.ElementTree as ET
 import urllib3
 import json
-
 from bs4 import BeautifulSoup
 from jinja2 import Environment, FileSystemLoader
 
@@ -26,6 +24,7 @@ TEAMPLATE_CATEOGRY = 'category.xml'
 TEMPLATE_PRODUCT = 'product.xml'
 
 URL_CATEGORIES = f'{API_URL}/categories'
+URL_PRODUCTS = f'{API_URL}/products'
 
 API_KEY = os.environ.get("PRESTASHOP_KEY") # you can hardcode your api_key 
 
@@ -92,23 +91,89 @@ class Product:
     A class representing a product  in the store.
     """
     template = env.get_template(TEMPLATE_PRODUCT)
-    
+    def __init__(self, id: int, name: str, price:any, img_path: str, category: Category, description: str):
+        self.id = id
+        self.name = name
+        self.price = price
+        self.img_path = img_path
+        self.category = category
+        self.descritpion = description
 
-def create_category_tree(data): 
+    def to_dict(self):
+        """
+        Converts the Product object to a dictionary.
+        """
+        return {
+            "id": self.id,
+            "name": self.name,
+            "price": self.price,
+            "img_path": self.img_path,
+            "category": self.category.id
+        }
+    def to_json(self):
+        """
+        Converts the Product object to a JSON string.
+        """
+        return json.dumps(self.to_dict(), ensure_ascii=False)
+    def to_xml(self):
+        """
+        Generates XML for the product using the Jinja2 template.
+        """
+        product_data = self.to_dict()
+        return Product.template.render(product=product_data)
+    def send(self):
+        response = requests.post(API_URL+"/products", auth=auth, verify=False, data=self.to_xml())
+        if response.status_code == 201:
+            soup = BeautifulSoup(response.text, 'xml')
+            self.id=soup.find('id').text.strip()
+            print(f"Successfully created product with ID {self.id}")
+        else:
+            print(f"Failed to create product with ID {self.id}: {response.status_code} - {response.text}")        
+
+class Product_feature:
     """
-    Creates a tree structure of categories and subcategories.
+    A class representing products feature.
+    """
+    def __init__(self, id: int , name: str):
+        self.id = id
+        self.name = name
+
+class Product_featur_value:
+    """
+    A class representig value of product feature
+    """
+    def __init__(self, id: int , value: str, feature: Product_feature):
+        self.id = id
+        self.value = value
+        self.featue = feature
+
+def create(data): 
+    """
+    Creates a tree structure of categories, subcategories and products with features.
     """
     categories = []
     for category_name, category_data in data.items():
         category = Category(id=-1, id_parent=2, name=category_name)
-        category.send()
+        # category.send()
         categories.append(category)
 
-        for sub_category_name in category_data.get('subcategories', {}):
+        for sub_category_name, sub_category_data in category_data.get('subcategories', {}).items():
             sub_category = Category(id=len(categories)+3, id_parent=category.id, name=sub_category_name)
-            sub_category.send()
+            # sub_category.send()
             categories.append(sub_category)
-    return categories
+            for product_name, products_data in sub_category_data.get('products', {}).items():
+                print(product_name)
+                create_products_for_category(category=sub_category, products=products_data)
+    
+
+def create_products_for_category(category: Category, products):
+    """
+    products jest obiektem przechowującym wszystkie dane trzeba sparsowac, na podstawie "images" wyciagnac z niego sciezke img oraz na podstawie "other_qualities" tworzyć product_features (trzeba sprawdzić czy juz nie zostaly wczesniej takie stworzone product_features i product_feature_values)
+    """
+    product = Product(id=-1, name=products['product_name'], price=products['product_name'], img_path="", category=category, description=products['product_description'])
+    print(product.to_json())
+    
+
 
 if __name__ == "__main__": 
     with open('../data/data.json', 'r',encoding='utf-8') as file:
@@ -117,6 +182,7 @@ if __name__ == "__main__":
     # formatted_data = format_json_with_depth(data, max_depth=6)
     # print(json.dumps(formatted_data, indent=4, ensure_ascii=False))
     
-    categories = create_category_tree(data)        
+    categories = create(data)        
+
 
 
