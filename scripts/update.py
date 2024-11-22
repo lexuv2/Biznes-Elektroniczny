2a32344f9b99a26ee0beab91bad8266a7e9b3a38
lexuv2 <alex2003gd@gmail.com>
2024-11-22 22:31:28 +0100

    fixed SOME bugs in update.py, still broken

--- scripts/update.py
+++ scripts/update.py
@@ -19,7 +19,7 @@ Wymagania:
 
 API_URL = "https://127.0.0.1/api"
 
-TEMPLATE_DIR = "./xml_templates/"
+TEMPLATE_DIR = "./scripts/xml_templates/"
 TEAMPLATE_CATEOGRY = 'category.xml'
 TEMPLATE_PRODUCT = 'product.xml'
 
@@ -27,6 +27,7 @@ URL_CATEGORIES = f'{API_URL}/categories'
 URL_PRODUCTS = f'{API_URL}/products'
 
 API_KEY = os.environ.get("PRESTASHOP_KEY") # you can hardcode your api_key 
+API_KEY = "AQCQDLJ5RLC4LD4X9V9LKZM1U9PFJ82E"
 
 env = Environment(loader=FileSystemLoader(TEMPLATE_DIR), autoescape=True)
 auth = requests.auth.HTTPBasicAuth(API_KEY, "")
@@ -78,7 +79,7 @@ class Category:
             return Category.template.render(category=category_data) 
     
     def send(self):
-        response = requests.post(API_URL+"/categories", auth=auth, verify=False, data=self.to_xml())
+        response = requests.post(API_URL+"/categories", auth=auth, verify=False, data=self.to_xml().encode('utf-8'))
         if response.status_code == 201:
             soup = BeautifulSoup(response.text, 'xml')
             self.id=soup.find('id').text.strip()
@@ -94,7 +95,7 @@ class Product:
     def __init__(self, id: int, name: str, price:any, img_path: str, category: Category, description: str):
         self.id = id
         self.name = name
-        self.price = price
+        self.price = float(price.replace('\xa0zł', '').replace(',', '.'))
         self.img_path = img_path
         self.category = category
         self.descritpion = description
@@ -122,12 +123,13 @@ class Product:
         product_data = self.to_dict()
         return Product.template.render(product=product_data)
     def send(self):
-        response = requests.post(API_URL+"/products", auth=auth, verify=False, data=self.to_xml())
+        response = requests.post(API_URL+"/products", auth=auth, verify=False, data=self.to_xml().encode('utf-8'))
         if response.status_code == 201:
             soup = BeautifulSoup(response.text, 'xml')
             self.id=soup.find('id').text.strip()
             print(f"Successfully created product with ID {self.id}")
         else:
+            print(self.to_xml())
             print(f"Failed to create product with ID {self.id}: {response.status_code} - {response.text}")        
 
 class Product_feature:
@@ -154,29 +156,36 @@ def create(data):
     categories = []
     for category_name, category_data in data.items():
         category = Category(id=-1, id_parent=2, name=category_name)
-        # category.send()
+        category.send()
         categories.append(category)
 
         for sub_category_name, sub_category_data in category_data.get('subcategories', {}).items():
             sub_category = Category(id=len(categories)+3, id_parent=category.id, name=sub_category_name)
-            # sub_category.send()
+            sub_category.send()
             categories.append(sub_category)
             for product_name, products_data in sub_category_data.get('products', {}).items():
-                print(product_name)
-                create_products_for_category(category=sub_category, products=products_data)
+                # print(product_name)
+                # create_products_for_category(category=sub_category, products=products_data)
+                product = Product(id=1, name=product_name, price=products_data['product_price'], img_path=products_data['images'][0]['url'], category=sub_category, description=products_data['product_description'])
+                print(product.to_json())
+                product.send()
     
 
 def create_products_for_category(category: Category, products):
     """
     products jest obiektem przechowującym wszystkie dane trzeba sparsowac, na podstawie "images" wyciagnac z niego sciezke img oraz na podstawie "other_qualities" tworzyć product_features (trzeba sprawdzić czy juz nie zostaly wczesniej takie stworzone product_features i product_feature_values)
     """
-    product = Product(id=-1, name=products['product_name'], price=products['product_name'], img_path="", category=category, description=products['product_description'])
-    print(product.to_json())
+    ## iterate over products dictionary
+    for product_data in products.items():
+        print(product_data)
+
+    # product = Product(id=-1, name=products['product_name'], price=products['price'], img_path=products['images'][0], category=category, description=products['description'])
+    # print(product.to_json())
     
 
 
 if __name__ == "__main__": 
-    with open('../data/data.json', 'r',encoding='utf-8') as file:
+    with open('./data/data.json', 'r',encoding='utf-8') as file:
         data = json.load(file)
 
     # formatted_data = format_json_with_depth(data, max_depth=6)
