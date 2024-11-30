941cd02f0d05c6cd25f30e408803b256d34dd7cb
Hubert Wajda <hubert.wajda03@gmail.com>
2024-11-30 16:10:25 +0100

    configs

--- prestashop/root/var/cache/prod/smarty/compile/classic/a9/d4/96/a9d496f00a4f81df42699c4952850153d0f01d26_0.module.psmboviewstemplateshookre.php
+++ prestashop/root/var/cache/prod/smarty/compile/classic/a9/d4/96/a9d496f00a4f81df42699c4952850153d0f01d26_0.module.psmboviewstemplateshookre.php
@@ -1,18 +1,18 @@
 <?php
-/* Smarty version 3.1.43, created on 2024-11-30 12:07:58
+/* Smarty version 3.1.43, created on 2024-11-30 16:01:39
   from 'module:psmboviewstemplateshookre' */
 
 /* @var Smarty_Internal_Template $_smarty_tpl */
 if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   'version' => '3.1.43',
-  'unifunc' => 'content_674af20e239912_76011953',
+  'unifunc' => 'content_674b28d3abf001_33227097',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
     'a9d496f00a4f81df42699c4952850153d0f01d26' => 
     array (
       0 => 'module:psmboviewstemplateshookre',
-      1 => 1732961289,
+      1 => 1732978824,
       2 => 'module',
     ),
   ),
@@ -20,7 +20,7 @@ if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   array (
   ),
 ),false)) {
-function content_674af20e239912_76011953 (Smarty_Internal_Template $_smarty_tpl) {
+function content_674b28d3abf001_33227097 (Smarty_Internal_Template $_smarty_tpl) {
 echo '<script'; ?>
 >
   if (undefined !== mbo) {
