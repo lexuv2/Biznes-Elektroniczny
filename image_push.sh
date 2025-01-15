87c6e915f7e47cfe7af28c77008ac44217403486
Mikolaj Klikowicz <mikolaj.klikowicz@gmail.com>
2025-01-15 22:08:56 +0100

    cleanup for deployment

--- image_push.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-docker image tag wloczki3:latest tirex003/weloczki3:tagname
-docker push tirex003/weloczki3:tagname
--- prestashop/root/modules/paypal/views/js/order_confirmation.js
+++
@@ -1,3 +0,0 @@
-/*! For license information please see order_confirmation.js.LICENSE.txt */
-$(document).ready((function(){return $(".page-order-confirmation #order-details ul").append($("[data-paypal-transaction-id]"))}));
-//# sourceMappingURL=order_confirmation.js.map
